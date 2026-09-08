84a8e9d925cd8dbfb6e24c35aa06f643a959ed93
Lim Cheng Yang <chengyang1017@gmail.com>
2026-09-08 21:38:37 +0800

    feat: stub local folder picker

--- lib/features/export/services/workspace_import_picker_stub.dart
+++ lib/features/export/services/workspace_import_picker_stub.dart
@@ -1,5 +1,9 @@
 import 'dart:typed_data';
 
 const bool supportsWorkspaceImportPicker = false;
+const bool supportsWorkspaceDirectoryPicker = false;
 
 Future<Uint8List?> pickWorkspaceImport() async => null;
+
+Future<List<({String path, Uint8List bytes})>?> pickWorkspaceDirectory() async =>
+    null;
