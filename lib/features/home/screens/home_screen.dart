b1de3c78ffd162129bdba62ad89dedae4a511b35
Lim Cheng Yang <chengyang1017@gmail.com>
2026-09-12 13:58:27 +0800

    Show Flutter Workbench branding on home screen

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -88,7 +88,7 @@ class HomeScreen extends StatelessWidget {
                           ),
                           const SizedBox(height: 20),
                           Text(
-                            'Flutter UI Playground',
+                            'Flutter Workbench',
                             textAlign: TextAlign.center,
                             style: Theme.of(context).textTheme.headlineMedium,
                           ),
